6d3172750b193f7cd5821911eeea1d6baccec05b
Eljah <eljah@mail.ru>
2020-04-03 10:28:14 +0300

    more correct lines comparision

--- src/main/java/SeleniumCopyPaste.java
+++ src/main/java/SeleniumCopyPaste.java
@@ -79,6 +79,7 @@ public class SeleniumCopyPaste {
                     //System.out.println("Processed: " + processeds + ", " + processeds.length());
                 }
                 ;
+                internal:
                 for (String founts : fount) {
                     boolean flag = true;
                     for (String processeds : processed) {
@@ -89,7 +90,7 @@ public class SeleniumCopyPaste {
                     ;
                     if (flag) {
                         foundURL = founts;
-                        break;
+                        break internal;
                     }
                 }
                 ;
